0cada48902ae7793fbf70d5427e733d12fe5ed40
marc <marc.hoeffl@gmail.com>
2017-08-08 16:33:11 +0200

    Replace complete Taxation logic

--- src/pricing/command/calculate-tax.js
+++ src/pricing/command/calculate-tax.js
@@ -1,14 +1,53 @@
 const Joi = require('joi')
+const patrun = require('patrun')
+const _ = require('lodash')
 
 const pattern = {
   topic: 'tax',
   cmd: 'calculate',
-  net: Joi.number().required()
+  net: Joi.number().required(),
+  country: Joi.string(),
+  state: Joi.string(),
+  city: Joi.string(),
+  type: Joi.string(),
 }
 
+function I(val) { var rate = function () { return val }; rate.val = val; return rate }
+
+var salestax = patrun()
+salestax
+  // For Backwards Compatibility, if called without additional data
+  // return Swedish Tax rate
+  .add({}, I(0.23))
+
+  .add({ country: 'IE' }, I(0.25))
+  .add({ country: 'UK' }, I(0.20))
+  .add({ country: 'SE' }, I(0.23))
+  .add({ country: 'DE' }, I(0.19))
+
+  .add({ country: 'IE', type: 'reduced' }, I(0.135))
+  .add({ country: 'IE', type: 'food' }, I(0.048))
+
+  .add({ country: 'UK', type: 'food' }, I(0.0))
+
+  .add({ country: 'DE', type: 'reduced' }, I(0.07))
+
+  .add({ country: 'US' }, I(0.0)) // no federeal rate (yet!)
+
+  .add({ country: 'US', state: 'AL' }, I(0.04))
+  .add({ country: 'US', state: 'AL', city: 'Montgomery' }, I(0.10))
+
+  .add({ country: 'US', state: 'NY' }, I(0.07))
+  .add({ country: 'US', state: 'NY', type: 'reduced' }, function under110(net) {
+    return net < 110 ? 0.0 : salestax.find({ country: 'US', state: 'NY' })
+  })
+
 function handler(msg, reply) {
-  const rate = 0.23
-  const total = msg.net * (1 + rate)
+  const taxPattern = _.omit(msg, 'topic', 'cmd', 'net')
+  const calculate = salestax.find(taxPattern)
+
+  const rate = calculate(msg.net)
+  const total = Math.round(msg.net * (1 + rate) * 100) / 100
   reply(null, { total: total })
 }
 
--- src/pricing/spec/command/calculate-tax.spec.js
+++ src/pricing/spec/command/calculate-tax.spec.js
@@ -1,12 +1,88 @@
 const expect = require('unexpected')
 const { handler: calculate } = require('../../command/calculate-tax')
 
-it('Calculates the total', (done) => {
+it('Calculates the total (default: Sweden)', (done) => {
   const net = 100;
   calculate({ net }, (err, res) => {
     expect(err, 'not to be ok')
     const { total } = res;
     expect(total, 'to be', 123)
+    done()
+  })
+})
+
+it('Standard Rate in Sweden', (done) => {
+  const net = 100;
+  calculate({ country: 'SE', net }, (err, res) => {
+    expect(err, 'not to be ok')
+    const { total } = res;
+    expect(total, 'to be', 123)
+    done()
+  })
+})
+
+
+it('Standard rate in Ireland on E99:', (done) => {
+  calculate({ country: 'IE', net: 100 }, (err, res) => {
+    expect(err, 'not to be ok')
+    const { total } = res;
+    expect(total, 'to be', 125)
+    done()
+  })
+})
+
+
+it('Food rate in Ireland on E99:', (done) => {
+  calculate({ country: 'IE', type: 'food', net: 100 }, (err, res) => {
+    expect(err, 'not to be ok')
+    const { total } = res;
+    expect(total, 'to be', 104.8)
+    done()
+  })
+})
+
+
+it('Reduced rate in Ireland', (done) => {
+  calculate({ country: 'IE', type: 'reduced', net: 100 }, (err, res) => {
+    expect(err, 'not to be ok')
+    const { total } = res;
+    expect(total, 'to be', 113.5)
+    done()
+  })
+})
+
+it('Default Rate in Germany', (done) => {
+  calculate({ country: 'DE', net: 100 }, (err, res) => {
+    expect(err, 'not to be ok')
+    const { total } = res;
+    expect(total, 'to be', 119)
+    done()
+  })
+})
+
+it('Standard rate in Alabama', (done) => {
+  calculate({ country: 'US', state: 'AL', net: 100 }, (err, res) => {
+    expect(err, 'not to be ok')
+    const { total } = res;
+    expect(total, 'to be', 104)
+    done()
+  })
+})
+
+it('Standard rate in Montgomery, Alabama', (done) => {
+  calculate({ country: 'US', state: 'AL', city: 'Montgomery', net: 100 }, (err, res) => {
+    expect(err, 'not to be ok')
+    const { total } = res;
+    expect(total, 'to be', 110)
+    done()
+  })
+})
+
+it('Reduced rate in New York for clothes', (done) => {
+  calculate({ country: 'US', state: 'NY', type: 'reduced', net: 100 }, (err, res) => {
+    expect(err, 'not to be ok')
+    const { total } = res;
+    expect(total, 'to be', 100)
     done()
   })
 })
